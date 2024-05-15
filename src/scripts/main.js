fec6313f13e060070a2d1c7b6ee62e05ee1d0599
Luiz Francisco <luizfk.gmail.com>
2024-05-14 22:46:51 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('ola mundo')
+AOS.init();
+
+const dataDoEvento = new Date("May 24, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciAteOEvento / diaEmMs)
+    const horasAteOEvento = Math.floor((distanciAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciAteOEvento % minutoEmMs) / 1000);
+
+    document.querySelector('.contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.querySelector('.contador').innerHTML = 'Evento Expirado'
+    }
+}, 1000);
